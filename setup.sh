c2e60f5a9b87c55443e5aade6ad3ce6be35ac7f2
Kári Bertilsson <kari@zenter.is>
2016-12-27 15:01:22 +0000

    Install oh-my-zsh & liquidprompt

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-#zsh
-ln -sf `pwd`/zshrc ~/.zshrc
+#install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+#install liquidprompt
+cd ~
+git clone https://github.com/nojhan/liquidprompt.git
 
 # vim
 ln -sf `pwd`/vim/vimrc ~/.vimrc
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#zsh
+ln -sf `pwd`/zshrc ~/.zshrc
